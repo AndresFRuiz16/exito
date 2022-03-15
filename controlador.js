57c1de7969aca21de387d992e64a8a7e31300111
Andres <felipermko@gmail.com>
2022-03-15 17:16:39 -0500

    se anade js

---
+++ controlador.js
@@ -0,0 +1,12 @@
+let etiquetaBoton=document.getElementById("boton");
+etiquetaBoton.addEventListener("click",escuchar);
+function escuchar(event){
+    event.preventDefault()
+    console.log("hizo click")
+    let procesador=document.getElementById("procesador").value
+    console.log(procesador)
+    let edad=document.getElementById("Edad").value
+    console.log(edad)
+    let cantidad=document.getElementById("cantidad").value
+    console.log(cantidad)
+}
